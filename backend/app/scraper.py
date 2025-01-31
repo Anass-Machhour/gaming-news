d34dc1b4b509d9779409c8108ce80009e5210d98
RegularNobody <70586742+RegularNobody@users.noreply.github.com>
2025-01-31 13:54:27 +0000

    Refactor scraping

--- backend/app/scraper.py
+++ backend/app/scraper.py
@@ -2,7 +2,6 @@ import aiohttp
 from bs4 import BeautifulSoup as bs
 from datetime import datetime, timezone
 from urllib.parse import urljoin
-from .database import SessionLocal
 from .models import Website, Article
 
 
@@ -26,6 +25,7 @@ async def scrape_website(website_data, db):
 
             website = db.query(Website).filter_by(
                 url=website_data["url"]).first()
+            
             if not website:
                 # Create a new website entry
                 website = Website(
@@ -35,61 +35,76 @@ async def scrape_website(website_data, db):
                     created_at=datetime.now(timezone.utc)
                 )
                 db.add(website)
-                db.commit()
 
-            # Select the articles section
-            section_tag, section_class = website_data["section_selector"][
-                "tag"], website_data["section_selector"]["class"]
-            main_section = soup.find(
-                section_tag,
-                class_=section_class
-            )
-
-            # Check if the section exist
-            if main_section:
-                # Select the articles URL
-                article_tag, article_class = website_data["article_selector"][
-                    "tag"], website_data["article_selector"]["class"]
-                articles = soup.find_all(article_tag, article_class)
-                # Check if articles is not empty
-                if articles:
-                    for article in articles:
-                        # Get the article full URL, from href tag and join it with website URL
-                        get_href = article.find("a")
-                        link = get_href["href"]
-                        article_url = urljoin(website_data["url"], link)
-
-                        # Redirecting to the article page
-                        html = await fetch(session, article_url)
-                        soup = bs(html, "html.parser")
-
-                        # Get the headline
-                        headline_tag = soup.find("h1")
-                        headline = headline_tag.text.strip()
-
-                        # Select the thumbnail tag
-                        thumbnail_selector = soup.find(
-                            website_data["thumbnail_selector"]["tag"], website_data["thumbnail_selector"]["class"])
-                        thumbnail_tag = thumbnail_selector.find("img")
-                        # extract the thumbnail URL
-                        thumbnail = thumbnail_tag.get(
-                            "data-src", thumbnail_tag.get("src", "No thumbnail"))
-
-                        # Check if the article already exists in database
-                        if not db.query(Article).filter_by(url=article_url).first():
-                            article_db = Article(
-                                url=article_url,
-                                headline=headline,
-                                thumbnail_url=thumbnail,
-                                website_id=website.id,
-                                created_at=datetime.now(timezone.utc)
-                            )
-                            db.add(article_db)
-
-            # Commit all changes to the database
-            db.commit()
+            await scrape_articles(website_data, website.id, session, db)
 
     except Exception as e:
         # In case of an error will Rollback
         db.rollback()
         print(f"An error occurred scraping {website_data["url"]}: {e}")
+
+
+async def scrape_articles(website_data, websiteID, session, db):
+    try:
+        html = await fetch(session, website_data["url"])
+        soup = bs(html, "html.parser")
+        
+        # Select the articles section
+        section_tag, section_class = website_data["section_selector"][
+            "tag"], website_data["section_selector"]["class"]
+
+        main_section = soup.find(section_tag, class_=section_class)
+
+        # Check if the section exist
+        if main_section:
+            # Select the articles URL
+            article_tag, article_class = website_data["article_selector"][
+                "tag"], website_data["article_selector"]["class"]
+            
+            articles = soup.find_all(article_tag, article_class)
+
+            # Check if articles is not empty
+            if articles:
+                for article in articles:
+                    # Get the article full URL, from href tag and join it with website URL
+                    get_href = article.find("a")
+                    link = get_href["href"]
+                    article_url = urljoin(website_data["url"], link)
+
+                    # Redirecting to the article page
+                    html = await fetch(session, article_url)
+                    soup = bs(html, "html.parser")
+
+                    # Get the headline
+                    headline_tag = soup.find("h1")
+                    headline = headline_tag.text.strip()
+
+                    # Select the thumbnail tag
+                    thumbnail_selector = soup.find(
+                        website_data["thumbnail_selector"]["tag"], website_data["thumbnail_selector"]["class"])
+                    thumbnail_tag = thumbnail_selector.find("img")
+
+                    # extract the thumbnail URL
+                    thumbnail = thumbnail_tag.get(
+                        "data-src", thumbnail_tag.get("src", "No thumbnail"))
+
+                    # Check if the article already exists in database
+                    if not db.query(Article).filter_by(url=article_url).first():
+                        article_db = Article(
+                            url=article_url,
+                            headline=headline,
+                            thumbnail_url=thumbnail,
+                            website_id=websiteID,
+                            created_at=datetime.now(timezone.utc)
+                        )
+                        db.add(article_db)
+            else:
+                print("Articles not found")
+        else:
+            print("Section not found")
+
+    # In case of an error will Rollback
+    except Exception as e:
+        db.rollback()
+        print(f"error occurred scraping articles from {
+              website_data["url"]}: {e}")
--- backend/app/tasks.py
+++ backend/app/tasks.py
@@ -3,7 +3,6 @@ import os
 from celery import Celery, shared_task
 from celery.schedules import crontab
 from .database import SessionLocal
-from .models import Website, Article
 from .scraper import scrape_website
 
 # Initialize Celery
@@ -22,28 +21,21 @@ app.conf.update(
     enable_utc=True,
 )
 
-
-@shared_task
-def scrape_website_task(websites):
-    for website in websites:
-        async def run_scrape():
-            db = SessionLocal()
-            try:
-                await scrape_website(website, db)
-            except Exception as e:
-                print(f"Error scraping {website["url"]}: {e}")
-            finally:
-                db.close()
-
-        asyncio.run(run_scrape())
+# Trigger Celery beat tasks every 8 hours
+app.conf.beat_schedule = {
+    'scrape-every-8hours': {
+        'task': 'app.tasks.scrape_website_task',
+        'schedule': crontab(minute=0, hour="*/8"),
+    },
+}
 
 
 websites = [
     {
         "name": "kotaku",
-        "url": "https://kotaku.com/latest",
+        "url": "https://kotaku.com/culture/news",
         "section_selector": {"tag": "div", "class": "sc-17uq8ex-0 fakHlO"},
-        "article_selector": {"tag": "div", "class": "sc-3kpz0l-2 ciHPAq"},
+        "article_selector": {"tag": "div", "class": "sc-cw4lnv-12 kQoJyO"},
         "headline_selector": "h1",
         "thumbnail_selector": {"tag": "div", "class": "sc-1eow4w5-3 hGpdBg"},
     },
@@ -58,11 +50,18 @@ websites = [
 ]
 
 
-# Trigger Celery beat tasks every minute
-app.conf.beat_schedule = {
-    'scrape-every-hour': {
-        'task': 'app.tasks.scrape_website_task',
-        'schedule': crontab(minute=0, hour="*/8"),
-        'args': (websites,),
-    },
-}
+@shared_task
+def scrape_website_task():
+    db = SessionLocal()
+    try:
+        for website in websites:
+            asyncio.run(scrape_website(website, db))
+            db.commit()
+    except Exception as e:
+        db.rollback()
+        print(f"Error occur {website["url"]}: {e}")
+    finally:
+        db.close()
+
+
+scrape_website_task.delay()
